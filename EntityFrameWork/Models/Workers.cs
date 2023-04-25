d405cc1f18158e651f9195afdf65034fb839e4de
[shaigak] <shaigak@code.edu.az>
2023-04-26 02:59:11 +0300

    task2

--- EntityFrameWork/Models/Workers.cs
+++ EntityFrameWork/Models/Workers.cs
@@ -1,10 +1,16 @@
-﻿namespace EntityFrameWork.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameWork.Models
 {
     public class Workers:BaseEntity
     {
         public string? Name { get; set; }
 
         public string? Image { get; set; }
+
+        [NotMapped]
+        public IFormFile Photo { get; set; }
+
 
         public string? Position { get; set; }
     }
