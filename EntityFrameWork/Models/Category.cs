fd64594f97025cd77142f53cc256348c19b847fe
[shaigak] <shaigak@code.edu.az>
2023-04-26 01:04:34 +0300

    new

---
+++ EntityFrameWork/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameWork.Models
+{
+    public class Category:BaseEntity
+    {
+        [Required(ErrorMessage ="Dony be Empty ")]
+        //[StringLength(10,ErrorMessage ="The name length is not corrext")]
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
